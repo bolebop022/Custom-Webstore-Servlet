2ebe97fb71b34b2aa648d5bad8bdf151b112990c
Bongane Lebopo <bonganelebop@gmail.com>
2025-04-11 11:26:05 +0200

    Servlet init defined.

--- src/main/java/org/blindustries/Dispatcher.java
+++ src/main/java/org/blindustries/Dispatcher.java
@@ -1,5 +1,26 @@
 package org.blindustries;
 
+import org.blindustries.Controllers.ProductController;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 @WebServlet
 public class Dispatcher extends HttpServlet{
+    private Map<String, Method> routeMappings = new HashMap<>();
+
+    @Override
+    public void init() {
+        scanControllers();
+    }
+
+    private void scanControllers(){
+        try{
+            routeMappings.put("/products", ProductController.class.getMethod("listProducts",
+                    HttpServeletRequest.class, HttpServeletResponse.class));
+        } catch (NoSuchMethodException e){
+            e.printStackTrace();
+        }
+    }
 }
